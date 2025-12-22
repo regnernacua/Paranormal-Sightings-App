24c8df8732bf81ceb28b013b7a0fd9be342955c5
regnernacua <nacua.regner@gmail.com>
2025-12-22 17:53:11 +0800

    Add createTable.js, logTable.js, and seedTable.js to set up and populate database

---
+++ server/controllers/sightingsController.js
@@ -0,0 +1,12 @@
+import { getDBConnection } from '../db/db.js'
+
+export async function getSightings(req, res) {
+
+  try {
+    const db = await getDBConnection()
+
+    
+  } catch (error) {
+    
+  }
+}
