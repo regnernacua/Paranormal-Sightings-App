92c2eda523adec810e3b424c1aa7d754f31e5a10
regnernacua <nacua.regner@gmail.com>
2026-04-17 07:52:24 +0800

    Update .gitignore

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import { sightingsRouter } from './routes/sightings.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT || 1337;
+
+// CORS (allow Vercel + Local)
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://paranormal-sightings-frontend.vercel.app/'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 
 app.use(express.json());
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -19,8 +32,15 @@ mongoose
     console.error('MongoDB connection error:', error);
   });
 
+// API routes
 app.use('/api/sightings', sightingsRouter);
 
+//Health check
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
